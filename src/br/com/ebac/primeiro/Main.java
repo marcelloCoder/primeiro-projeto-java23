a4110bdbd7696a82ce45e7fb2743b40c94146d38
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2023-01-18 19:32:10 -0300

    first commit

---
+++ src/br/com/ebac/primeiro/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package br.com.ebac.primeiro;
+
+/**
+ * @author marce
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+		/*String ambiente = args[0];
+		
+		
+		if (ambiente.equals("Dev")) {
+			System.out.println("Dev");
+		}*/
+
+	}
+
+}
